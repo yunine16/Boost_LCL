52bc115de64a67fcaf0a4a6b0817d8f1f3aeae30
yunine16 <yuchin616@icloud.com>
2021-03-14 15:21:05 +0900

    CarrierWaveをローカルで実装

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end
